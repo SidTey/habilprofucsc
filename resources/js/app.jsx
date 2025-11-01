4297cbad4b1af05d353be538ad7840e74fc71919
SHMEBUU <nalvarado@ing.ucsc.cl>
2025-10-31 21:49:09 -0300

    esto lo modifique para que no tome la testpage que estaba y tome el formulario de la funcionalidad 2

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './bootstrap';
-import TestPage from './pages/TestPage';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../css/app.css'; 
+import HabilprofForm from './components/HabilprofForm.jsx'; 
 
 function App() {
     return (
-        <div className="min-h-screen flex items-center justify-center">
-            <TestPage />
+        <div className="min-vh-100 d-flex align-items-center justify-content-center bg-light">            
+            <HabilprofForm />
+
         </div>
     );
 }
