00e6cd79fba1d7b559f5aa53fa3940d161cc10be
SHMEBUU <nalvarado@ing.ucsc.cl>
2025-10-31 21:49:36 -0300

    edite el web para agregar las direcciones correctas y asi tener todo ordenado, comente tambien el route::get para que esta sea la de login

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Página de inicio - Aquí irá el login';
+});
+
+Route::get('/habilitacion/agregar', function () {
+    return view('AgregarHabilitacion');
 });
