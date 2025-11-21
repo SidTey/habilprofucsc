e8fb6b52dc753c8e523fed8dc1fa48edbf72dba4
SHMEBUU <nalvarado@ing.ucsc.cl>
2025-11-20 21:00:07 -0300

    modifique backup segun retroalimentacion

--- database/backup/habilprof.sql
+++ database/backup/habilprof.sql
@@ -2,12 +2,12 @@
 -- PostgreSQL database dump
 --
 
-\restrict gYJcmgM2ZpBBGyJtqGlP9RiegzxUsJcr1RW8o8iNkfSqASTrJpTcV14fUVdpleg
+\restrict zkiNsWuw0SllfsPeWenfSQGOIfJDMBgGgBn0ZkIxbWfapMkWaWiA3gxVXkGdyCH
 
 -- Dumped from database version 17.6
 -- Dumped by pg_dump version 17.6
 
--- Started on 2025-11-20 19:45:35
+-- Started on 2025-11-20 20:58:12
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -39,12 +39,12 @@ COMMENT ON SCHEMA public IS 'standard public schema';
 
 
 --
--- TOC entry 886 (class 1247 OID 48222)
+-- TOC entry 883 (class 1247 OID 48819)
 -- Name: tipo_rol; Type: TYPE; Schema: public; Owner: -
 --
 
 CREATE TYPE public.tipo_rol AS ENUM (
-    'Profesor Guia',
+    'Profesor_Guia',
     'Profesor_Co_Guia',
     'Profesor_Comision',
     'Profesor_Tutor'
@@ -56,7 +56,7 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
--- TOC entry 217 (class 1259 OID 48111)
+-- TOC entry 217 (class 1259 OID 48735)
 -- Name: alumno; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -68,30 +68,31 @@ CREATE TABLE public.alumno (
 
 
 --
--- TOC entry 218 (class 1259 OID 48116)
+-- TOC entry 227 (class 1259 OID 48827)
 -- Name: asigna; Type: TABLE; Schema: public; Owner: -
 --
 
 CREATE TABLE public.asigna (
     id_habilitacion integer NOT NULL,
     rut_profesor bigint NOT NULL,
-    rol public.tipo_rol NOT NULL
+    rol character varying(255),
+    tipo_rol public.tipo_rol
 );
 
 
 --
--- TOC entry 219 (class 1259 OID 48119)
--- Name: autentificacion_de_usuarios; Type: TABLE; Schema: public; Owner: -
+-- TOC entry 218 (class 1259 OID 48740)
+-- Name: autentificacion_de_usuario; Type: TABLE; Schema: public; Owner: -
 --
 
-CREATE TABLE public.autentificacion_de_usuarios (
+CREATE TABLE public.autentificacion_de_usuario (
     rut_admin bigint NOT NULL,
     "contraseña" character varying(255) NOT NULL
 );
 
 
 --
--- TOC entry 220 (class 1259 OID 48122)
+-- TOC entry 219 (class 1259 OID 48743)
 -- Name: empresa; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -102,7 +103,7 @@ CREATE TABLE public.empresa (
 
 
 --
--- TOC entry 221 (class 1259 OID 48125)
+-- TOC entry 220 (class 1259 OID 48746)
 -- Name: habilitacion_profesional; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -110,15 +111,15 @@ CREATE TABLE public.habilitacion_profesional (
     id_habilitacion integer NOT NULL,
     rut_alumno bigint NOT NULL,
     descripcion_habilitacion text NOT NULL,
-    nota_final real,
+    nota_final numeric,
     fecha_nota date,
-    "año" integer NOT NULL,
-    semestre integer NOT NULL
+    "año_semestre" integer NOT NULL,
+    numero_semestre integer NOT NULL
 );
 
 
 --
--- TOC entry 222 (class 1259 OID 48130)
+-- TOC entry 221 (class 1259 OID 48751)
 -- Name: habilitacion_profesional_id_habilitacion_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
@@ -133,7 +134,7 @@ CREATE SEQUENCE public.habilitacion_profesional_id_habilitacion_seq
 
 --
 -- TOC entry 4972 (class 0 OID 0)
--- Dependencies: 222
+-- Dependencies: 221
 -- Name: habilitacion_profesional_id_habilitacion_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
@@ -141,7 +142,7 @@ ALTER SEQUENCE public.habilitacion_profesional_id_habilitacion_seq OWNED BY publ
 
 
 --
--- TOC entry 223 (class 1259 OID 48131)
+-- TOC entry 222 (class 1259 OID 48752)
 -- Name: pring; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -152,7 +153,7 @@ CREATE TABLE public.pring (
 
 
 --
--- TOC entry 224 (class 1259 OID 48134)
+-- TOC entry 223 (class 1259 OID 48755)
 -- Name: prinv; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -163,7 +164,7 @@ CREATE TABLE public.prinv (
 
 
 --
--- TOC entry 225 (class 1259 OID 48137)
+-- TOC entry 224 (class 1259 OID 48758)
 -- Name: profesor; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -175,7 +176,7 @@ CREATE TABLE public.profesor (
 
 
 --
--- TOC entry 226 (class 1259 OID 48142)
+-- TOC entry 225 (class 1259 OID 48763)
 -- Name: prtut; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -187,7 +188,7 @@ CREATE TABLE public.prtut (
 
 
 --
--- TOC entry 227 (class 1259 OID 48145)
+-- TOC entry 226 (class 1259 OID 48766)
 -- Name: supervisor; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -199,7 +200,7 @@ CREATE TABLE public.supervisor (
 
 
 --
--- TOC entry 4781 (class 2604 OID 48148)
+-- TOC entry 4781 (class 2604 OID 48769)
 -- Name: habilitacion_profesional id_habilitacion; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -207,7 +208,7 @@ ALTER TABLE ONLY public.habilitacion_profesional ALTER COLUMN id_habilitacion SE
 
 
 --
--- TOC entry 4955 (class 0 OID 48111)
+-- TOC entry 4955 (class 0 OID 48735)
 -- Dependencies: 217
 -- Data for Name: alumno; Type: TABLE DATA; Schema: public; Owner: -
 --
@@ -217,28 +218,29 @@ COPY public.alumno (rut_alumno, nombre_alumno, correo_alumno) FROM stdin;
 
 
 --
--- TOC entry 4956 (class 0 OID 48116)
--- Dependencies: 218
+-- TOC entry 4965 (class 0 OID 48827)
+-- Dependencies: 227
 -- Data for Name: asigna; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-COPY public.asigna (id_habilitacion, rut_profesor, rol) FROM stdin;
+COPY public.asigna (id_habilitacion, rut_profesor, rol, tipo_rol) FROM stdin;
 \.
 
 
 --
--- TOC entry 4957 (class 0 OID 48119)
+-- TOC entry 4956 (class 0 OID 48740)
+-- Dependencies: 218
+-- Data for Name: autentificacion_de_usuario; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+COPY public.autentificacion_de_usuario (rut_admin, "contraseña") FROM stdin;
+12345678	$2y$12$QJtA8Gyd.aPHfWcs4j9c5et01.EcZrDxL7lqQeCeZd.YV5yYLOUvq
+\.
+
+
+--
+-- TOC entry 4957 (class 0 OID 48743)
 -- Dependencies: 219
--- Data for Name: autentificacion_de_usuarios; Type: TABLE DATA; Schema: public; Owner: -
---
-
-COPY public.autentificacion_de_usuarios (rut_admin, "contraseña") FROM stdin;
-\.
-
-
---
--- TOC entry 4958 (class 0 OID 48122)
--- Dependencies: 220
 -- Data for Name: empresa; Type: TABLE DATA; Schema: public; Owner: -
 --
 
@@ -247,18 +249,18 @@ COPY public.empresa (rut_empresa, nombre_empresa) FROM stdin;
 
 
 --
--- TOC entry 4959 (class 0 OID 48125)
--- Dependencies: 221
+-- TOC entry 4958 (class 0 OID 48746)
+-- Dependencies: 220
 -- Data for Name: habilitacion_profesional; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-COPY public.habilitacion_profesional (id_habilitacion, rut_alumno, descripcion_habilitacion, nota_final, fecha_nota, "año", semestre) FROM stdin;
+COPY public.habilitacion_profesional (id_habilitacion, rut_alumno, descripcion_habilitacion, nota_final, fecha_nota, "año_semestre", numero_semestre) FROM stdin;
 \.
 
 
 --
--- TOC entry 4961 (class 0 OID 48131)
--- Dependencies: 223
+-- TOC entry 4960 (class 0 OID 48752)
+-- Dependencies: 222
 -- Data for Name: pring; Type: TABLE DATA; Schema: public; Owner: -
 --
 
@@ -267,8 +269,8 @@ COPY public.pring (id_habilitacion, titulo_proy) FROM stdin;
 
 
 --
--- TOC entry 4962 (class 0 OID 48134)
--- Dependencies: 224
+-- TOC entry 4961 (class 0 OID 48755)
+-- Dependencies: 223
 -- Data for Name: prinv; Type: TABLE DATA; Schema: public; Owner: -
 --
 
@@ -277,8 +279,8 @@ COPY public.prinv (id_habilitacion, titulo_proy) FROM stdin;
 
 
 --
--- TOC entry 4963 (class 0 OID 48137)
--- Dependencies: 225
+-- TOC entry 4962 (class 0 OID 48758)
+-- Dependencies: 224
 -- Data for Name: profesor; Type: TABLE DATA; Schema: public; Owner: -
 --
 
@@ -287,8 +289,8 @@ COPY public.profesor (rut_profesor, nombre_profesor, correo_profesor) FROM stdin
 
 
 --
--- TOC entry 4964 (class 0 OID 48142)
--- Dependencies: 226
+-- TOC entry 4963 (class 0 OID 48763)
+-- Dependencies: 225
 -- Data for Name: prtut; Type: TABLE DATA; Schema: public; Owner: -
 --
 
@@ -297,8 +299,8 @@ COPY public.prtut (id_habilitacion, rut_empresa, rut_supervisor) FROM stdin;
 
 
 --
--- TOC entry 4965 (class 0 OID 48145)
--- Dependencies: 227
+-- TOC entry 4964 (class 0 OID 48766)
+-- Dependencies: 226
 -- Data for Name: supervisor; Type: TABLE DATA; Schema: public; Owner: -
 --
 
@@ -308,15 +310,15 @@ COPY public.supervisor (rut_supervisor, nombre_supervisor, rut_empresa) FROM std
 
 --
 -- TOC entry 4973 (class 0 OID 0)
--- Dependencies: 222
+-- Dependencies: 221
 -- Name: habilitacion_profesional_id_habilitacion_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.habilitacion_profesional_id_habilitacion_seq', 1, true);
+SELECT pg_catalog.setval('public.habilitacion_profesional_id_habilitacion_seq', 1, false);
 
 
 --
--- TOC entry 4783 (class 2606 OID 48150)
+-- TOC entry 4783 (class 2606 OID 48771)
 -- Name: alumno alumno_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -325,7 +327,7 @@ ALTER TABLE ONLY public.alumno
 
 
 --
--- TOC entry 4785 (class 2606 OID 48152)
+-- TOC entry 4801 (class 2606 OID 48831)
 -- Name: asigna asigna_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -334,16 +336,16 @@ ALTER TABLE ONLY public.asigna
 
 
 --
--- TOC entry 4787 (class 2606 OID 48154)
--- Name: autentificacion_de_usuarios autentificacion_de_usuarios_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+-- TOC entry 4785 (class 2606 OID 48773)
+-- Name: autentificacion_de_usuario autentificacion_de_usuarios_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
-ALTER TABLE ONLY public.autentificacion_de_usuarios
+ALTER TABLE ONLY public.autentificacion_de_usuario
     ADD CONSTRAINT autentificacion_de_usuarios_pkey PRIMARY KEY (rut_admin);
 
 
 --
--- TOC entry 4789 (class 2606 OID 48156)
+-- TOC entry 4787 (class 2606 OID 48775)
 -- Name: empresa empresa_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -352,7 +354,7 @@ ALTER TABLE ONLY public.empresa
 
 
 --
--- TOC entry 4791 (class 2606 OID 48158)
+-- TOC entry 4789 (class 2606 OID 48777)
 -- Name: habilitacion_profesional habilitacion_profesional_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -361,7 +363,7 @@ ALTER TABLE ONLY public.habilitacion_profesional
 
 
 --
--- TOC entry 4793 (class 2606 OID 48160)
+-- TOC entry 4791 (class 2606 OID 48779)
 -- Name: pring pring_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -370,7 +372,7 @@ ALTER TABLE ONLY public.pring
 
 
 --
--- TOC entry 4795 (class 2606 OID 48162)
+-- TOC entry 4793 (class 2606 OID 48781)
 -- Name: prinv prinv_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -379,7 +381,7 @@ ALTER TABLE ONLY public.prinv
 
 
 --
--- TOC entry 4797 (class 2606 OID 48164)
+-- TOC entry 4795 (class 2606 OID 48783)
 -- Name: profesor profesor_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -388,7 +390,7 @@ ALTER TABLE ONLY public.profesor
 
 
 --
--- TOC entry 4799 (class 2606 OID 48166)
+-- TOC entry 4797 (class 2606 OID 48785)
 -- Name: prtut prtut_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -397,7 +399,7 @@ ALTER TABLE ONLY public.prtut
 
 
 --
--- TOC entry 4801 (class 2606 OID 48168)
+-- TOC entry 4799 (class 2606 OID 48787)
 -- Name: supervisor supervisor_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -406,7 +408,7 @@ ALTER TABLE ONLY public.supervisor
 
 
 --
--- TOC entry 4802 (class 2606 OID 48169)
+-- TOC entry 4808 (class 2606 OID 48832)
 -- Name: asigna asigna_id_habilitacion_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -415,7 +417,7 @@ ALTER TABLE ONLY public.asigna
 
 
 --
--- TOC entry 4803 (class 2606 OID 48174)
+-- TOC entry 4809 (class 2606 OID 48837)
 -- Name: asigna asigna_rut_profesor_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -424,7 +426,7 @@ ALTER TABLE ONLY public.asigna
 
 
 --
--- TOC entry 4804 (class 2606 OID 48179)
+-- TOC entry 4802 (class 2606 OID 48788)
 -- Name: habilitacion_profesional habilitacion_profesional_rut_alumno_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -433,34 +435,34 @@ ALTER TABLE ONLY public.habilitacion_profesional
 
 
 --
--- TOC entry 4805 (class 2606 OID 48184)
+-- TOC entry 4803 (class 2606 OID 48793)
 -- Name: pring pring_id_habilitacion_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.pring
-    ADD CONSTRAINT pring_id_habilitacion_fkey FOREIGN KEY (id_habilitacion) REFERENCES public.habilitacion_profesional(id_habilitacion);
+    ADD CONSTRAINT pring_id_habilitacion_fkey FOREIGN KEY (id_habilitacion) REFERENCES public.habilitacion_profesional(id_habilitacion) ON DELETE CASCADE;
 
 
 --
--- TOC entry 4806 (class 2606 OID 48189)
+-- TOC entry 4804 (class 2606 OID 48798)
 -- Name: prinv prinv_id_habilitacion_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.prinv
-    ADD CONSTRAINT prinv_id_habilitacion_fkey FOREIGN KEY (id_habilitacion) REFERENCES public.habilitacion_profesional(id_habilitacion);
+    ADD CONSTRAINT prinv_id_habilitacion_fkey FOREIGN KEY (id_habilitacion) REFERENCES public.habilitacion_profesional(id_habilitacion) ON DELETE CASCADE;
 
 
 --
--- TOC entry 4807 (class 2606 OID 48194)
+-- TOC entry 4805 (class 2606 OID 48803)
 -- Name: prtut prtut_id_habilitacion_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.prtut
-    ADD CONSTRAINT prtut_id_habilitacion_fkey FOREIGN KEY (id_habilitacion) REFERENCES public.habilitacion_profesional(id_habilitacion);
+    ADD CONSTRAINT prtut_id_habilitacion_fkey FOREIGN KEY (id_habilitacion) REFERENCES public.habilitacion_profesional(id_habilitacion) ON DELETE CASCADE;
 
 
 --
--- TOC entry 4808 (class 2606 OID 48199)
+-- TOC entry 4806 (class 2606 OID 48808)
 -- Name: prtut prtut_rut_empresa_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -469,7 +471,7 @@ ALTER TABLE ONLY public.prtut
 
 
 --
--- TOC entry 4809 (class 2606 OID 48204)
+-- TOC entry 4807 (class 2606 OID 48813)
 -- Name: prtut prtut_rut_supervisor_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -477,11 +479,11 @@ ALTER TABLE ONLY public.prtut
     ADD CONSTRAINT prtut_rut_supervisor_fkey FOREIGN KEY (rut_supervisor) REFERENCES public.supervisor(rut_supervisor);
 
 
--- Completed on 2025-11-20 19:45:35
+-- Completed on 2025-11-20 20:58:12
 
 --
 -- PostgreSQL database dump complete
 --
 
-\unrestrict gYJcmgM2ZpBBGyJtqGlP9RiegzxUsJcr1RW8o8iNkfSqASTrJpTcV14fUVdpleg
+\unrestrict zkiNsWuw0SllfsPeWenfSQGOIfJDMBgGgBn0ZkIxbWfapMkWaWiA3gxVXkGdyCH
 
